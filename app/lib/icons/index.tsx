8b4d4e8711d13953278da980b482a43d6c8d2624
Richard Nemeth <nemethricsi@gmail.com>
2024-08-25 21:27:22 +0200

    add custom icons

---
+++ app/lib/icons/index.tsx
@@ -0,0 +1,45 @@
+import Add from '@/public/icons/add.svg';
+import BackArrow from '@/public/icons/back-arrow.svg';
+import Call from '@/public/icons/call.svg';
+import Change from '@/public/icons/change.svg';
+import Delete from '@/public/icons/delete.svg';
+import Favourite from '@/public/icons/favourite.svg';
+import LightMode from '@/public/icons/light-mode.svg';
+import More from '@/public/icons/more.svg';
+import Mute from '@/public/icons/mute.svg';
+import Search from '@/public/icons/search.svg';
+import Settings from '@/public/icons/settings.svg';
+
+export const AddIcon = ({ className }: { className?: string }) => {
+  return <Add className={className} />;
+};
+export const BackArrowIcon = ({ className }: { className?: string }) => {
+  return <BackArrow className={className} />;
+};
+export const CallIcon = ({ className }: { className?: string }) => {
+  return <Call className={className} />;
+};
+export const ChangeIcon = ({ className }: { className?: string }) => {
+  return <Change className={className} />;
+};
+export const DeleteIcon = ({ className }: { className?: string }) => {
+  return <Delete className={className} />;
+};
+export const FavouriteIcon = ({ className }: { className?: string }) => {
+  return <Favourite className={className} />;
+};
+export const LightModeIcon = ({ className }: { className?: string }) => {
+  return <LightMode className={className} />;
+};
+export const MoreIcon = ({ className }: { className?: string }) => {
+  return <More className={className} />;
+};
+export const MuteIcon = ({ className }: { className?: string }) => {
+  return <Mute className={className} />;
+};
+export const SearchIcon = ({ className }: { className?: string }) => {
+  return <Search className={className} />;
+};
+export const SettingsIcon = ({ className }: { className?: string }) => {
+  return <Settings className={className} />;
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    );
+
+    if (fileLoaderRule) {
+      const resourceQueryNot = fileLoaderRule.resourceQuery?.not || [];
+
+      config.module.rules.push(
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/,
+        },
+        {
+          test: /\.svg$/i,
+          issuer: fileLoaderRule.issuer,
+          resourceQuery: { not: [...resourceQueryNot, /url/] },
+          use: ['@svgr/webpack'],
+        },
+      );
+
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
