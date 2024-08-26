d65223bdc66eb16f78f10558c50be272c36a263e
Richard Nemeth <nemethricsi@gmail.com>
2024-08-26 19:28:59 +0200

    let ProfileMenuButton receive button props

---
+++ app/ui/profile-menu-button.tsx
@@ -0,0 +1,22 @@
+import type { ButtonProps } from '@headlessui/react';
+import { Button } from '@headlessui/react';
+import Image from 'next/image';
+
+const ProfileMenuButton = (props: ButtonProps) => {
+  return (
+    <Button
+      className="button-base secondary-button padding-icon-only-button"
+      {...props}
+    >
+      <Image
+        src="/images/user.png"
+        alt="user avatar"
+        width={24}
+        height={24}
+        className="rounded-full border-[1.5px] border-white"
+      />
+    </Button>
+  );
+};
+
+export default ProfileMenuButton;
