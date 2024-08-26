0fe31a4eca3dcd9d2110288e0275ccc0e96a1603
Richard Nemeth <nemethricsi@gmail.com>
2024-08-26 19:02:08 +0200

    add Settings button to HeaderActions

--- app/ui/header-actions.tsx
+++ app/ui/header-actions.tsx
@@ -1,12 +1,13 @@
 'use client';
 
+import Button from '@/app/ui/button';
 import SpecialButton from '@/app/ui/special-button';
 
 const HeaderActions = () => {
   return (
     <div className="flex items-center gap-6">
       <div className="flex items-center gap-2">
-        <button>settings</button>
+        <Button variant="secondary" iconId="settings" />
         <button>user menu</button>
       </div>
       <SpecialButton
