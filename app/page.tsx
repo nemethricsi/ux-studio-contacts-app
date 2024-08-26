c967687b1da6d9f4f58d7f8632c8c6275628c790
Richard Nemeth <nemethricsi@gmail.com>
2024-08-26 14:14:39 +0200

    add SpecialButton to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import SpecialButton from '@/app/ui/special-button';
 import Text from '@/app/ui/text';
 
 export default function Home() {
   return (
     <>
-      <Text variant="h1">Contacts</Text>
+      <header className="mx-auto flex max-w-[768px] items-center justify-between border border-grey-60 p-6">
+        <Text variant="h1">Contacts</Text>
+        <Actions />
+      </header>
+      <main className="mx-auto max-w-[768px] border border-grey-60 px-6 py-3">
+        <Text variant="h3">Contact List</Text>
+      </main>
     </>
   );
 }
+
+const Actions = () => {
+  return <SpecialButton label="Add new" />;
+};
