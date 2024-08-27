68143fb1de7e690098d62176617865c0d87283ba
Richard Nemeth <nemethricsi@gmail.com>
2024-08-27 15:24:10 +0200

    add empty state

--- app/ui/contact-list/contact-list.tsx
+++ app/ui/contact-list/contact-list.tsx
@@ -2,6 +2,7 @@
 
 import { useContacts } from '@/app/hooks/useContacts';
 import Text from '@/app/ui/text';
+import Image from 'next/image';
 import ContactListItem from './contact-list-item';
 
 const ContactList = () => {
@@ -13,7 +14,17 @@ const ContactList = () => {
     <>
       {isLoading && <Text variant="h2">Loading...</Text>}
       {!isLoading && contacts?.length === 0 && (
-        <Text variant="h3">No contacts found.</Text>
+        <div className="mt-4 flex flex-col items-center gap-4">
+          <Image
+            src="/images/add-contact.svg"
+            alt="empty state illustration"
+            width={200}
+            height={200}
+          />
+          <Text variant="h3" className="text-secondary text-center">
+            No contacts yet. Add a new contact to get started!
+          </Text>
+        </div>
       )}
       {!isLoading &&
         contacts?.map((contact) => (
