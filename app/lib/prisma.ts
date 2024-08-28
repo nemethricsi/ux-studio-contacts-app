5dbaa8e0db8660a1ed543bdb7881b5da63f56928
Richard Nemeth <nemethricsi@gmail.com>
2024-08-28 11:46:26 +0200

    apply prisma extension

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,5 +1,6 @@
+import { enforceNonEmptyNameExtension } from '@/app/lib/prismaExtensions';
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient().$extends(enforceNonEmptyNameExtension);
 
 export default prisma;
