ed03bca8f0d73d47387bb1c5639fb03b01c28c76
Richard Nemeth <nemethricsi@gmail.com>
2024-08-28 15:46:36 +0200

    open edit form from contact list item

--- app/ui/contact-list/contact-list-item-actions.tsx
+++ app/ui/contact-list/contact-list-item-actions.tsx
@@ -1,12 +1,12 @@
 import Button from '@/app/ui/button';
 import DropdownMenu from '@/app/ui/dropdown-menu';
 
-const ContactListItemActions = ({ contactId }: { contactId: string }) => {
+const ContactListItemActions = ({ contactId, openEditModal }: { contactId: string; openEditModal: () => void }) => {
   return (
     <div className="flex items-center gap-2">
       <Button label="Mute" variant="secondary" iconId="mute" title="Mute contact" iconOnly />
       <Button label="Call" variant="secondary" iconId="call" title="Call contact" iconOnly />
-      <DropdownMenu contactId={contactId} />
+      <DropdownMenu contactId={contactId} openEditModal={openEditModal} />
     </div>
   );
 };
--- app/ui/contact-list/contact-list-item.tsx
+++ app/ui/contact-list/contact-list-item.tsx
@@ -1,41 +1,58 @@
 'use client';
 
 import ContactListItemActions from '@/app/ui/contact-list/contact-list-item-actions';
+import EditForm from '@/app/ui/form/edit-form';
+import Modal from '@/app/ui/modal';
 import Text from '@/app/ui/text';
 import type { Contact } from '@prisma/client';
 import Image from 'next/image';
 import { useState } from 'react';
 
-const ContactListItem = ({ contact: { id, name, imageUrl, phoneNumber } }: { contact: Contact }) => {
+const ContactListItem = ({ contact }: { contact: Contact }) => {
+  const { id, name, imageUrl, phoneNumber } = contact;
   const [hoveredRow, setHoveredRow] = useState<string | null>(null);
+  const [isOpen, setisOpen] = useState(false);
+
+  const openEditModal = () => {
+    setisOpen(true);
+  };
+
+  const closeEditModal = () => {
+    setisOpen(false);
+  };
 
   return (
-    <div
-      className="flex items-center justify-between gap-3 py-3"
-      onMouseEnter={() => setHoveredRow(id)}
-      onMouseLeave={() => setHoveredRow(null)}
-    >
-      <div className="flex items-center gap-4">
-        <div className="rounded-full bg-grey-70 p-px">
-          <Image
-            src={imageUrl ? imageUrl : '/images/default.png'}
-            alt="default avatar"
-            width={38}
-            height={38}
-            className="rounded-full"
-          />
+    <>
+      <div
+        className="flex items-center justify-between gap-3 py-3"
+        onMouseEnter={() => setHoveredRow(id)}
+        onMouseLeave={() => setHoveredRow(null)}
+      >
+        <div className="flex items-center gap-4">
+          <div className="rounded-full bg-grey-70 p-px">
+            <Image
+              src={imageUrl ? imageUrl : '/images/default.png'}
+              alt="default avatar"
+              width={38}
+              height={38}
+              className="rounded-full"
+            />
+          </div>
+          <div className="flex flex-col">
+            <Text variant="h3">{name}</Text>
+            {phoneNumber && (
+              <Text variant="message" className="text-secondary">
+                {phoneNumber}
+              </Text>
+            )}
+          </div>
         </div>
-        <div className="flex flex-col">
-          <Text variant="h3">{name}</Text>
-          {phoneNumber && (
-            <Text variant="message" className="text-secondary">
-              {phoneNumber}
-            </Text>
-          )}
-        </div>
+        {hoveredRow === id && <ContactListItemActions contactId={id} openEditModal={openEditModal} />}
       </div>
-      {hoveredRow === id && <ContactListItemActions contactId={id} />}
-    </div>
+      <Modal isOpen={isOpen} closeModal={closeEditModal}>
+        <EditForm contact={contact} />
+      </Modal>
+    </>
   );
 };
 
