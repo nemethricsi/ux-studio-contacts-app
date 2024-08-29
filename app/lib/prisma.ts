3928ae9b84141db1f3254b9c7d4452a703f16c15
Richard Nemeth <nemethricsi@gmail.com>
2024-08-29 10:53:06 +0200

    add enforceNonEmptyNameExtension to PrismaClient

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,3 +1,4 @@
+import { enforceNonEmptyNameExtension } from '@/app/lib/prismaExtensions';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 import { Pool } from 'pg';
@@ -6,6 +7,6 @@ const connectionString = process.env.DATABASE_URL!;
 
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({ adapter }).$extends(enforceNonEmptyNameExtension);
 
 export default prisma;
